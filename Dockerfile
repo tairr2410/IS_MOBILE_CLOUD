a3a7b349a5a55250d32e40015b80f764746b208e
Tair Rahamim <tairr@mellanox.com>
2019-07-02 11:57:39 +0300

    first command

---
+++ Dockerfile
@@ -0,0 +1,40 @@
+FROM php:7.2-apache
+
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends gnupg apt-transport-https unixodbc-dev && \
+    apt-get purge -y --auto-remove && \
+    rm -rf /var/lib/apt/lists/*
+
+# install odbc fro sql server
+RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
+    curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
+    apt-get update && \
+    ACCEPT_EULA=Y apt-get install -y --no-install-recommends msodbcsql17 && \
+    apt-get purge -y --auto-remove && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN pecl install sqlsrv pdo_sqlsrv > /dev/null && \
+        rm -rf /tmp/pear ~/.pearrc
+
+# create a directory for database config
+RUN mkdir -p -m 777 /var/www/config/
+
+# copy configuration files
+#COPY conf/mobile.conf /etc/apache2/sites-enabled/000-default.conf
+COPY conf/mobile.ini /usr/local/etc/php/conf.d/
+COPY conf/database.ini /var/www/config/
+
+# set up work dir
+WORKDIR /var/www/html/
+
+# create a directory for logs
+RUN mkdir -p -m 777 /var/log/mobile/
+
+# copy source code
+COPY src/ /var/www/html/
+
+# expose port 80
+#EXPOSE 80
+
+# start apache automatically
+CMD ["apache2-foreground"]
---
+++ src/index.php
@@ -0,0 +1,39 @@
+<?php
+require_once (__DIR__."/functions.php");
+
+defined ( 'APPLICATION_ENV' ) || define ( 'APPLICATION_ENV', (getenv ( 'APPLICATION_ENV' ) ? getenv ( 'APPLICATION_ENV' ) : 'development') );
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+echo "Hello, Tair! :)" . PHP_EOL . PHP_EOL;
+echo "Trying to connect to sql server in azure..." . PHP_EOL . PHP_EOL;
+
+
+$iconfigAll = parse_ini_file_extended("/var/www/config/database.ini");
+$iconfig = $iconfigAll[APPLICATION_ENV];
+
+$conn = "mobiletst";
+$host = $iconfig["database.{$conn}.params.host"];
+$port = $iconfig["database.{$conn}.params.port"];
+$username = $iconfig["database.{$conn}.params.username"];
+$password = $iconfig["database.{$conn}.params.password"];
+$sid = $iconfig["database.{$conn}.params.dbname"];
+
+$connectionInfo = array("UID" => $username, "pwd" => $password, "Database" => $sid, "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:$host,$port";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+if ($conn == FALSE){
+    var_dump(sqlsrv_errors());
+    die;
+}
+$sql = "select top 10 tblAuthors.* from tblAuthors";
+$getResults = sqlsrv_query($conn, $sql);
+echo "Reading data from table..." . PHP_EOL . PHP_EOL;
+if ($getResults == false){
+    var_dump(sqlsrv_errors());
+    die;
+}
+while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    echo $row['Id'] . " " . $row['Author_name'] . " " . $row['country'] . PHP_EOL;
+}
